cba8bfcf529807ab9e15052a4f49b61495ab2f3f
aboutroots <popiel.mw@gmail.com>
2019-07-25 22:24:11 +0200

    Prepare demo of vuex-router-sync

--- src/store.js
+++ src/store.js
@@ -1,10 +1,35 @@
-import Vue from "vue";
-import Vuex from "vuex";
-
-Vue.use(Vuex);
+import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './router'
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
-});
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    increment2(state) {
+      const oldValue = parseInt(state.route.query.counter2) || 0
+      router.push({
+        query: {
+          counter2: oldValue + 1
+        }
+      })
+    }
+  },
+  actions: {
+    increment({ commit }) {
+      setTimeout(() => {
+        commit('increment')
+      }, 400)
+    },
+    increment2({ commit }) {
+      setTimeout(() => {
+        commit('increment2')
+      }, 400)
+    }
+  }
+})
---
+++ src/views/EventShow.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Showing event #{{ id }}</h1>
+    <p>Not in route: counter: {{counter}}</p>
+    <p>In route: counter2: {{counter2}}</p>
+    <button @click="inc">INC COUNTER NOT IN ROUTE</button>
+    <button @click="inc2">INC COUNTER IN ROUTE</button>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+  props: ['id'],
+  computed: {
+    ...mapState({
+      counter: 'count',
+      counter2: state => state.route.query.counter2
+    })
+  },
+  methods: {
+    ...mapActions({
+      inc: 'increment',
+      inc2: 'increment2'
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
